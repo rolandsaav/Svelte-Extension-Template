472ea173905241df184ff89777f1441173477ab6
Roland Saavedra <rolandsaavedra22@gmail.com>
2023-11-02 17:42:52 -0400

    initial commit. Global styling setup to remove default margins

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<slot/>
+
+<style>
+	:global(*, *::after, *::before) {
+		margin: 0;
+		padding: 0;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+	<h1>Welcome to SvelteKit</h1>
+	<p>This is a template project for SvelteKit WebExtensions</p>
+</div>
+
+<style>
+	.container {
+		width: 300px;
+		outline: solid black 1px;
+		background-color: white;
+	}
+</style>
